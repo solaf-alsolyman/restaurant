469303572542fc8b01250ba99d28b92254f057f7
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-09-13 19:59:28 +0300

    add another commit

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
-
-// test commit
+/////////
+/////////
+/////////
+/////////
+/////////
+/////////
